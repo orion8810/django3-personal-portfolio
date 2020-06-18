02af7b597e43ba89baaff18322cc6bbc604f2980
David Carhart <dcarhart0505@gmail.com>
2020-06-17 21:56:03 -0400

    first

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dcarh\pycharmprojects\personal_portfolio\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
